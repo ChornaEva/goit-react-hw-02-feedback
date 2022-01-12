e9fd3693a3f4cbadcd0136130a4706cef5ff3c65
Chorna Yevheniia <chornaja.evgenija@gmail.com>
2022-01-12 11:32:08 +0200

    initial files

---
+++ src/components/Feedback.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Feedback.styled";
+// import propTypes from "prop-types";
+
+class Feedback extends React.Component {
+  render() {
+    return <div>hello</div>;
+  }
+}
+
+export default Feedback;
